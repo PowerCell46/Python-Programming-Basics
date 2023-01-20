828448d0e0cd1193532a7337a0795a69cf989263
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-01-20 21:34:52 +0200

    Create Multiplication Table.py

---
+++ Nested Loops Lab/Multiplication Table.py
@@ -0,0 +1,6 @@
+for first in range(1, 11):
+    current_first = first
+    for second in range(1, 11):
+        current_second = second
+        result = first * second
+        print(f'{current_first} * {current_second} = {result}')
