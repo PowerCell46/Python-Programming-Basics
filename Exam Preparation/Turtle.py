077c471e41091c8a1c50892b757375ca0bd3c399
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-02-01 22:49:27 +0200

    Create Turtle.py

---
+++ Exam Preparation/Turtle.py
@@ -0,0 +1,36 @@
+import turtle
+my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
+
+for i in range(0, 6):
+    my_turtle.left(60)
+    my_turtle.forward(200)
+
+
+turtle.done()
+
+
+import turtle
+my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
+
+for i in range(0, 6):
+    my_turtle.left(60)
+    my_turtle.forward(200)
+    my_turtle.left(120)
+    my_turtle.forward(200)
+    my_turtle.left(120)
+    my_turtle.forward(200)
+
+turtle.done()
+
+
+import turtle
+my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
+
+for i in range(0, 5):
+    my_turtle.forward(200)
+    my_turtle.left(144)
+
+turtle.done()
