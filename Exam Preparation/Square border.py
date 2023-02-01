64a5b82d5f504a16883de5ac689d105e91e82f73
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-02-01 22:52:06 +0200

    Create Square border.py

---
+++ Exam Preparation/Square border.py
@@ -0,0 +1,16 @@
+input_number = int(input("Enter the size of the side:"))
+counter = 0
+for i in range(0, input_number):
+    counter += 1
+    if counter == 1 or counter == input_number:
+        up_down_side = "+ "
+        for i in range(1, input_number - 1):
+            up_down_side += "- "
+        up_down_side += "+"
+        print(up_down_side)
+    else:
+        inside_side = "| "
+        for i in range(1, input_number - 1):
+            inside_side += "- "
+        inside_side += "|"
+        print(inside_side)
